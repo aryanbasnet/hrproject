5fb839ca8a28e71e6da2242b0a2155b6c50412cc
Roshan1277 <youaremyduplicate@gmail.com>
2025-11-30 19:39:06 -0500

    updated
    updated

---
+++ src/utils/roles.js
@@ -0,0 +1,5 @@
+export const roleAccess = {
+employee: ['/profile', '/onboarding', '/performance'],
+manager: ['/manager-dashboard', '/candidates', '/performance'],
+hr: ['/dashboard', '/jobs', '/candidates', '/onboarding', '/performance', '/profile'],
+};
